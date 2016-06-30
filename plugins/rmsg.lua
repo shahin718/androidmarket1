6b177854256615ec965eb4fe0b2ead0745d88df1
CleverBoy <mohammad.anishe@gmail.com>
2016-06-30 12:52:48 +0430

    Create rmsg.lua

---
+++ plugins/rmsg.lua
@@ -0,0 +1,31 @@
+local function history(extra, suc, result)
+  for i=1, #result do
+    delete_msg(result[i].id, ok_cb, false)
+  end
+  if tonumber(extra.con) == #result then
+    send_msg(extra.chatid, '"'..#result..' msg has been deleted', ok_cb, false)
+  else
+    send_msg(extra.chatid, 'massages are deleted', ok_cb, false)
+  end
+end
+local function run(msg, matches)
+  if matches[1] == 'rmsg' and is_owner(msg) then
+    if msg.to.type == 'channel' then
+      if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
+        return "Out of range!"
+      end
+      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+    else
+      return "In SuperGroups Only!"
+    end
+  else
+    return "You dont have access"
+  end
+end
+
+return {
+    patterns = {
+        '^[!/#](rmsg) (%d*)$'
+    },
+    run = run
+}
